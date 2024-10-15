09b7769ad9ae0009d3513fbd6d152351ecbe3444
liboxu0113 <liboxu0113@gmail.com>
2024-10-15 16:56:01 -0500

    update on data cleaning

--- code/cleaning/data_cleaning.py
+++ code/cleaning/data_cleaning.py
@@ -30,8 +30,8 @@ df_cleaned["mode"] = df["mode"]
 df_cleaned["popularity"] = df["popularity"]
 
 df_cleaned = df_cleaned.reset_index(drop=True)
-# df_cleaned.to_csv("artifacts/cleaned_data.csv", index=False)
 
+#Write into csv file
 with open("artifacts/cleaned_data.csv", mode="w", newline="", encoding="utf-8") as file:
     writer = csv.DictWriter(file, fieldnames=df_cleaned.columns)
     writer.writeheader()
