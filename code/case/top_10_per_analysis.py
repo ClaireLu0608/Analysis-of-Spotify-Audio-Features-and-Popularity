373dff97a81390157699f5a558690bb6ee3e95f6
Nicole Chan <nicole_chan@wireless-10-148-218-124.public.utexas.edu>
2024-10-15 23:15:55 -0500

    avg audio feature comparison update

---
+++ code/case/top_10_per_analysis.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+df = pd.read_csv('artifacts/cleaned_data.csv')
+
+# Step 1: Calculate the 90th percentile of the popularity score
+top_10_percent_threshold = df['popularity'].quantile(0.90)
+
+# Step 2: Create a new column indicating if the song is in the Top 10% or not
+df['top_10_percent'] = df['popularity'] >= top_10_percent_threshold
+
+# Step 3: Select the audio features to compare
+audio_features = ['danceability', 'tempo', 'liveness', 'instrumentalness','loudness','speechiness','key','mode','acousticness']
+
+# Step 4: Group the data by 'top_10_percent' and calculate the mean of the audio features for each group
+feature_means = df.groupby('top_10_percent')[audio_features].mean()
+
+# Step 5: Display the comparison
+print("Average Audio Features - Top 10% vs. Other Songs:")
+print(feature_means)
+
+import matplotlib.pyplot as plt
+
+feature_means.T.plot(kind='bar', figsize=(10,6))
+plt.title('Average Audio Features Comparison (Top 10% vs. Others)')
+plt.xlabel('Audio Features')
+plt.ylabel('Mean Value')
+plt.xticks(rotation=45)
+plt.savefig("images/avg_audio_features_comparison_top_10per.png", bbox_inches="tight")
+plt.show()
